7d82d7bb84681ba0ca5c2b3523f44903099714a6
itsmohmans <mohmans@pm.me>
2022-12-08 14:03:55 +0300

    fix: styling of the build banner

--- components/lib/buildInfoBanner.vue
+++ components/lib/buildInfoBanner.vue
@@ -1,34 +1,37 @@
 <template>
-  <div class="banner" v-if="!this.bannerClosed">
-    <div class="container">
-      <div class="flex items-center gap-x-4">
-        <span>🔨</span>
-        <p>
-          <span class="font-bold">Build</span>:
-          <a
-            :href="$config.DRONE_BUILD_LINK"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            #{{ $config.DRONE_BUILD_NUMBER }}
-          </a>
-          <a
-            :href="$config.DRONE_COMMIT_LINK"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ({{ $config.DRONE_COMMIT_SHA && $config.DRONE_COMMIT_SHA.slice(0, 6) }})
+  <div v-if="!this.bannerClosed">
+    <div class="banner">
+      <div class="main-container">
+        <div class="row-container">
+          <span>🔨</span>
+          <p>
+            <span class="font-bold">Build</span>:
+            <a
+              :href="$config.DRONE_BUILD_LINK"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              #{{ $config.DRONE_BUILD_NUMBER }}
             </a>
-        </p>
-      </div>
-      <div class="flex items-center gap-x-4">
-        <span>🚧</span>
-        <p> <span class="font-bold"> Environment: </span> {{ $config.TARGET_ENV }} </p>
-        <button class="bannerBtn" v-on:click="closeBanner">
-          &#215;
-        </button>
+            <a
+              :href="$config.DRONE_COMMIT_LINK"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ({{ $config.DRONE_COMMIT_SHA && $config.DRONE_COMMIT_SHA.slice(0, 6) }})
+              </a>
+          </p>
+        </div>
+        <div class="row-container">
+          <span>🚧</span>
+          <p> <span class="font-bold"> Environment: </span> {{ $config.TARGET_ENV }} </p>
+          <button class="bannerBtn" v-on:click="closeBanner">
+            &#215;
+          </button>
+        </div>
       </div>
     </div>
+    <div class="hidden-whitespace"></div>
   </div>
 </template>
 <script>
@@ -47,17 +50,17 @@ export default {
 }
 </script>
 <style scoped>
-.banner p,
-.banner a,
-.banner button {
-  color: white;
-}
 
-.bannerBtn{
-  font-weight: bold;
+.main-container,
+.row-container,
+.banner {
+  display: flex;
+  align-items: center;
 }
 
 .banner {
+  direction: ltr;
+  justify-content: space-between;
   background-color: #019ec3;
   width: 100%;
   height: 36px;
@@ -66,15 +69,29 @@ export default {
   top:0;
 }
 
-.container{
-  margin:0 auto 0 auto;
-  height: inherit;
-  padding: 0 40px 0 40px;
+.banner p,
+.banner a,
+.banner button {
+  color: white;
 }
 
-.banner .container {
-  display: flex;
+.main-container {
   justify-content: space-between;
-  align-items: center;
+  margin:0 auto;
+  height: inherit;
+  padding: 0 40px;
+  width: 100%;
+}
+
+.row-container {
+  column-gap: 0.5rem;
+}
+
+.bannerBtn {
+  font-size: 2rem;
+  font-weight: bold;
+}
+.hidden-whitespace {
+  height: 36px;
 }
 </style>
