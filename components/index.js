6ee89def269074f71026b5b7984751f33ef2ff03
Mohamed Mansour <mohmans@pm.me>
2023-06-04 16:29:46 +0300

    Remove Nuxt2-related files

--- components/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import components from './lib'
-// loop through components and register them
-for (const name in components) {
-  Vue.component(name, {
-    // extend the original component
-    extends: components[name],
-  })
-}
